4ae58ad6959010ef59e89bff6ea2491c8467ea9c
wingng <d1031022fcu@gmail.com>
2024-07-18 17:11:44 +0800

    在 training1 頁面加入 adder 範例

--- app/src/main/java/mobileapp/graduationproject/Training1.java
+++ app/src/main/java/mobileapp/graduationproject/Training1.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Training1 extends AppCompatActivity {
 
     private Button btnBack;
+    private Button btn_AdderCal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -28,5 +32,28 @@ public class Training1 extends AppCompatActivity {
         };
 
         btnBack.setOnClickListener(backHomeListener);
+    }
+
+    public void adderCal(View view){
+        // All the Operations can be done Here.
+        final TextView text=findViewById(R.id.adderResult);
+        final EditText valA = (EditText)findViewById(R.id.editTextNumberDecimalFirst);
+        final EditText valB = (EditText)findViewById(R.id.editTextNumberDecimalSecond);
+
+        if(valA.getText().toString().trim().length() > 0 &&
+                valB.getText().toString().trim().length() > 0) {
+
+            int numA = Integer.parseInt(valA.getText().toString());
+            int numB = Integer.parseInt(valB.getText().toString());
+
+            if(numA>0 && numA<100 && numB>0 && numB<100) {
+                text.setText("result = " + (numA+numB));
+            } else {
+                text.setText("please input 1-99");
+            }
+        } else {
+            text.setText("incorrect input");
+        }
+
     }
 }
